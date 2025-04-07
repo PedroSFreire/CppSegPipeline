8fd151584724905dc4c0780270a8754a001c503f
PedroSFreire <pedrofreire.freire@gmail.com>
2025-04-07 16:15:40 +0100

    Increase threshold for connected component removal
    Updated the threshold for removing connected components from 10 to 20. Introduced a new vector `count2` to track counts separately and added a loop to increment counts based on `imageBuffer` values. The removal of components is now based on the new threshold of 20. Additionally, modified the loop that processes `ccVec` to iterate one extra time, potentially affecting component indexing.

--- CCLAlgoAir.cpp
+++ CCLAlgoAir.cpp
@@ -34,20 +34,35 @@
 		}
 
 		for (int i = 0; i < objCount; i++) {
-			if (count[i] > 10) {
+			if (count[i] > 20) {
 				removeCC(i);
 				ccVec[i].size = 0;
 			}
 		}
+
+		std::vector<int> count2(objCount, 0);
+
+
 		for (int i = 0; i < xSize; i++) {
 			for (int j = 0; j < ySize; j++) {
 				int id = getIdFromPos(i, j, zSize-1);
 				if (imageBuffer[id] > 0) {
-					removeCC(imageBuffer[id]);
-					ccVec[imageBuffer[id]].size = 0;
+					count[imageBuffer[id]]++;
+					//removeCC(imageBuffer[id]);
+					//ccVec[imageBuffer[id]].size = 0;
 				}
 			}
 		}
+
+
+		for (int i = 0; i < objCount; i++) {
+			if (count2[i] > 20) {
+				removeCC(i);
+				ccVec[i].size = 0;
+			}
+		}
+
+
 		for (int i = 0; i < objCount+1; i++) {
 			if(ccVec[i].size>0)
 				f_ccVec.push_back(ccVec[i]);
