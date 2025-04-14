f08b81baf37c51e292baed538d09f756cac4a55a
Pedro Freire <pedrofreire.freire@gmail.com>
2025-04-14 19:22:27 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -26,6 +26,7 @@ echo Installing ITK, OpenCV, VLC, and VLC Core...
 "%VCPKG_DIR%\vcpkg" install itk:x64-windows
 "%VCPKG_DIR%\vcpkg" install opencv:x64-windows
 "%VCPKG_DIR%\vcpkg" install glew:x64-windows
+"%VCPKG_DIR%\vcpkg" install glfw3:x64-windows
 
 echo All packages installed successfully!
 pause
